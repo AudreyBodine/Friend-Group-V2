ffc682de4d23163437bab1ece1e4ea4e150230be
OwlNerd <aeb55s@missouristate.edu>
2022-11-08 22:08:58 -0600

    V2

---
+++ app/src/main/java/com/example/friendgroup/Friends.java
@@ -0,0 +1,48 @@
+package com.example.friendgroup;
+
+public class Friends {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Friends (int newId, String newFirstName, String newLastName, String newEmail){
+        setID(newId);
+        setFirstName(newFirstName);
+        setLastName(newLastName);
+        setEmail(newEmail);
+    }
+
+    private void setEmail(String newEmail) {
+        email = newEmail;
+    }
+
+    private void setLastName(String newLastName) {
+        lastName = newLastName;
+    }
+
+    private void setFirstName(String newFirstName) {
+        firstName = newFirstName;
+    }
+
+    private void setID(int newId) {
+        id = newId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String toString(){
+        return id + "; " + firstName + lastName + "; " + email;
+    }
+}
---
+++ app/src/main/java/com/example/friendgroup/InsertActivity.java
@@ -0,0 +1,52 @@
+package com.example.friendgroup;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InsertActivity extends AppCompatActivity {
+
+    private DatabaseManager dbManager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        dbManager = new DatabaseManager(this);
+        setContentView(R.layout.activity_insert);
+    }
+
+    public void insert(View view) {
+
+        // Retrieve name and email
+
+        Log.w("InsertActivity", "Insert Button Pushed");
+        EditText firstET = findViewById(R.id.first_nameET);
+        EditText lastET = findViewById(R.id.last_nameET);
+        EditText emailET = findViewById(R.id.emailET);
+        String fName = firstET.getText().toString();
+        String lName = lastET.getText().toString();
+        String emailString = emailET.getText().toString();
+
+        // Insert into database
+            Friends friend = new Friends(0, fName, lName, emailString);
+            dbManager.insert(friend);
+            Toast.makeText(this, "Friends Added", Toast.LENGTH_SHORT).show();
+
+        // Clear data
+        firstET.setText("");
+        lastET.setText("");
+        emailET.setText("");
+    }
+
+    public void goBack(View view) {
+        this.finish();
+
+    }
+}
+
+
